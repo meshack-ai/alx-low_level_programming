143f8e2fa85cfab7bb9b93c73960fee647a3a9c7
meshack-ai <kipngetichmeshack022@gmail.com>
2023-03-18 14:23:02 -0700

    Numbers task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  *main -Entry point
+  *Description - program to print single base ten numbers
+  *Return: Always 0
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
