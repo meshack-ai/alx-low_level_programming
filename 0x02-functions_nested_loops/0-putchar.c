a8f76734699f16a7e2ba3d023b34996754b0b6b0
meshack-ai <kipngetichmeshack022@gmail.com>
2023-03-21 11:41:55 -0700

    _putchar task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *Dscription - c program to print _putchar
+  *Return - always (0)
+  */
+
+int main(void)
+{
+	printf("_putchar");
+
+	return 0;
+}
