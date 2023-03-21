d8727053e2348b4b5fe7e70e4dcf2ddcfffa6aad
meshack-ai <kipngetichmeshack022@gmail.com>
2023-03-21 13:22:16 -0700

    10 x alphabet task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  *print_alphabet_x10- print alphabet ten times
+  *
+  *Return: Always 0 (Success)
+  */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+
+}
