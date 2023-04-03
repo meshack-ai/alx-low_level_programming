05030c9b2ab3b8267fdb0b25e38c9b380f58e4a9
meshack-ai <kipngetichmeshack022@gmail.com>
2023-04-03 13:15:45 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,29 @@
-#include "main.h"
-/**
- * _memset - fill a block of memory with a specific value
- * @s: starting address of memory to be filled
- * @b: the desired value
- * @n: number of bytes to be changed
- *
- * Return: changed array with new value for n bytes
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	int i = 0;
-
-	for (; n > 0; i++)
-	{
-		s[i] = b;
-		n--;
-	}
-	return (s);
+/*                                                                                                                                   
+ * File: 0-memset.c                                                                                                                  
+ * Auth: not                                                                                                                         
+ */                                                                                                                                  
+                                                                                                                                     
+#include "main.h"                                                                                                                    
+                                                                                                                                     
+/**                                                                                                                                  
+ * _memset - Fills the first n bytes of the memory area                                                                              
+ *          pointed to by @s with the constant byte @c.                                                                              
+ * @s: A pointer to the memory area to be filled.                                                                                    
+ * @c: The character to fill the memory area with.                                                                                   
+ * @n: The number of bytes to be filled.                                                                                             
+ * description _memset: over there                                                                                                   
+ *                                                                                                                                   
+ * Return: A pointer to the filled memory area @s.                                                                                   
+ *                                                                                                                                   
+ */                                                                                                                                  
+void *_memset(void *s, int c, size_t n)                                                                                              
+{                                                                                                                                    
+        unsigned int index;                                                                                                          
+        unsigned char *memory = s, value = c;                                                                                        
+                                                                                                                                     
+        for (index = 0; index < n; index++)                                                                                          
+                memory[index] = value;                                                                                               
+                                                                                                                                     
+        return (memory);                                                                                                             
 }
 
