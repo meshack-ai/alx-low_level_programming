caf769a3a818786ca201feb8bae7fa5043304d18
meshack-ai <kipngetichmeshack022@gmail.com>
2023-04-03 12:49:00 -0700

    memset task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,21 @@
 #include "main.h"
-
 /**
-  **_memset - programt o fill a constant byte
-  *@s: memory area to be filled
-  *@b: char to copy
-  *@n: number of times to copy s
-  *
-  *Return: pointer to the memory area s
-  */
+ * _memset - fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: changed array with new value for n bytes
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	int i = 0;
 
-	for (i = 0; i < n; i++)
+	for (; n > 0; i++)
 	{
 		s[i] = b;
+		n--;
 	}
-
 	return (s);
 }
+
